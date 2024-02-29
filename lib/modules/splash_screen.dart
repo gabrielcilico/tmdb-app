fe034d0d8ddc1aca34f77fe6a2879293818723b5
gabrielcilico <gabrielcilico@gmail.com>
2024-02-29 01:57:14 -0300

    fix: adjust splash screen

--- lib/modules/splash_screen.dart
+++ lib/modules/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_app/common/functions/navigator_service.dart';
+import 'package:tmdb_app/modules/movies/movies_module.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,6 +13,9 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    Future.delayed(const Duration(seconds: 2)).then((_) {
+      pushNamed(routeName: MoviesModule.moviesRoute);
+    });
   }
 
   @override
