ed39f44f643b8a74bab69b991a6ecdce531b2d11
gabrielcilico <gabrielcilico@gmail.com>
2024-02-29 01:57:00 -0300

    fix: adjust navigation to singleton

--- lib/common/navigation.dart
+++ lib/common/navigation.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 class NavigationService {
+  factory NavigationService() {
+    return _instance;
+  }
+
+  NavigationService._internal();
+
+  static final NavigationService _instance = NavigationService._internal();
+
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 }
