ed79b39a93aabb0d7202a81f5225f398416ec637
gabrielcilico <gabrielcilico@gmail.com>
2024-02-29 01:56:39 -0300

    feat: add navigator service

---
+++ lib/common/functions/navigator_service.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+void pushReplacementNamed({
+  required String routeName,
+  arguments,
+  bool forRoot = false,
+}) {
+  Modular.to.pushReplacementNamed(
+    routeName,
+    arguments: arguments,
+    forRoot: forRoot,
+  );
+}
+
+void pushNamed({
+  required String routeName,
+  arguments,
+}) {
+  Modular.to.pushNamed(
+    routeName,
+    arguments: arguments,
+  );
+}
+
+void pushNamedAndRemoveUntil({
+  required String routeName,
+  arguments,
+}) {
+  Modular.to.pushNamedAndRemoveUntil(
+    routeName,
+    (_) => true,
+    arguments: arguments,
+  );
+}
